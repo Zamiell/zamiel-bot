7164f3075de80050b5a658fb5828dfcf735631bd
Zamiell <5511220+Zamiell@users.noreply.github.com>
2023-01-13 17:03:14 +0000

    feat: stop script

---
+++ stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit on errors
+set -e
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pm2 stop zamiel-bot
