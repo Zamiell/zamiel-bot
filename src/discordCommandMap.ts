044a650630636827394af2e665a95a3e92f1059c
James <5511220+Zamiell@users.noreply.github.com>
2023-09-26 17:54:01 -0400

    fix: import

--- src/discordCommandMap.ts
+++ src/discordCommandMap.ts
@@ -1,11 +1,8 @@
 import type discord from "discord.js";
 import { ChannelType } from "discord.js";
-import isaacRacingCommon from "isaac-racing-common";
-import { ReadonlyMap, getRandomArrayElement } from "isaacscript-common-ts";
+import { ReadonlyMap } from "isaacscript-common-ts";
 import { CLIENT_LOBBY_CHANNEL_ID } from "./constants.js";
 import { discordSend } from "./discordUtils.js";
-
-const { BUILDS, CHARACTERS } = isaacRacingCommon;
 
 export const DISCORD_COMMAND_MAP = new ReadonlyMap<
   string,
@@ -23,8 +20,12 @@ function buildFunc(message: discord.Message) {
     return;
   }
 
+  /*
   const build = getRandomArrayElement(BUILDS);
   discordSend(message.channel, `Random build: ${build.name}`);
+  */
+
+  discordSend(message.channel, "This feature is temporarily unavailable.");
 }
 
 function characterFunc(message: discord.Message) {
@@ -32,8 +33,12 @@ function characterFunc(message: discord.Message) {
     return;
   }
 
+  /*
   const character = getRandomArrayElement(CHARACTERS);
   discordSend(message.channel, `Random character: ${character}`);
+  */
+
+  discordSend(message.channel, "This feature is temporarily unavailable.");
 }
 
 function helpFunc(message: discord.Message) {
