0e68ab06807cc068c2d9e6e4b77ae495e0952d61
James <5511220+Zamiell@users.noreply.github.com>
2023-11-28 23:32:31 -0500

    chore: remove t3 env

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,12 @@
-import { createEnv } from "@t3-oss/env-core";
 import dotenv from "dotenv";
-import fs from "node:fs";
+import { isFile } from "isaacscript-common-node";
 import path from "node:path";
 import { z } from "zod";
 import { REPO_ROOT } from "./constants.js";
 
 const ENV_PATH = path.join(REPO_ROOT, ".env");
 
-if (!fs.existsSync(ENV_PATH)) {
+if (!isFile(ENV_PATH)) {
   throw new Error(
     `The "${ENV_PATH}" file does not exist. Copy the ".env.example" file to a ".env" file at the root of the repository.`,
   );
@@ -17,21 +16,11 @@ dotenv.config({
   path: ENV_PATH,
 });
 
-// TODO: https://github.com/t3-oss/t3-env/issues/109
-// TODO: https://github.com/t3-oss/t3-env/pull/113
-for (const [key, value] of Object.entries(process.env)) {
-  if (value === "") {
-    delete process.env[key]; // eslint-disable-line @typescript-eslint/no-dynamic-delete
-  }
-}
-
-export const env = createEnv({
-  server: {
-    TWITCH_USERNAME: z.string(),
-    TWITCH_OAUTH: z.string().startsWith("oauth:"),
-    TWITCH_ADMIN_USERNAME: z.string(),
-    DISCORD_TOKEN: z.string(),
-  },
-
-  runtimeEnv: process.env,
+const envSchema = z.object({
+  TWITCH_USERNAME: z.string(),
+  TWITCH_OAUTH: z.string().startsWith("oauth:"),
+  TWITCH_ADMIN_USERNAME: z.string(),
+  DISCORD_TOKEN: z.string(),
 });
+
+export const env = envSchema.parse(process.env);
