4f9b7096bf17e823bada34d6155fc55abe4a513d
James <5511220+Zamiell@users.noreply.github.com>
2023-11-29 11:35:42 -0500

    fix: lint

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,5 @@
 import isaacRacingCommon from "isaac-racing-common";
-import { dirName } from "isaacscript-common-node";
-import path from "node:path";
 
-const __dirname = dirName();
-
-export const REPO_ROOT = path.join(__dirname, "..");
 export const COMMAND_PREFIX_TWITCH = "!";
 export const COMMAND_PREFIX_DISCORD = "/";
 export const CLIENT_LOBBY_CHANNEL_ID = "286115994621968384";
